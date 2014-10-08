8b32f2289152bdf74297e8d6b0706df5d3e844c4
fgarbagnati <f.g.garbagnati@gmail.com>
2014-10-08 16:55:26 -0700

    cleaned up comments, extra lines

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,11 +10,7 @@ class ContactsController < ApplicationController
   def send_message
     sh = SendHub.new
     puts params["phone_number"]
-
     puts sh.post_messages({:contacts => params["phone_number"], :text => params["messageBody"]})
-    # OpenSSL::SSL::SSLError (SSL_connect returned=1 errno=0 state=SSLv3 read server certificate B: certificate verify failed):
-    # app/controllers/contacts_controller.rb:14:in `send_message'
-
   end
 
   private
