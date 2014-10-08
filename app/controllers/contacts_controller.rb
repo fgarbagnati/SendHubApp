9ad00dc77aac463f4fb4ef72a7a22905865abf1c
fgarbagnati <f.g.garbagnati@gmail.com>
2014-10-08 00:34:19 -0700

    making some ajax things

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,14 @@
 class ContactsController < ApplicationController
   def index
   end
+
+  def create
+    @contact = Contact.create(contact_params)
+    render json: @contact
+  end
+
+  private
+  def contact_params
+    params.require(:contacts).permit(:name, :phone_number)
+  end
 end
