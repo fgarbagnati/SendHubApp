d83025a0f28a2117e8c1e9aeeb557eecd9f861bc
fgarbagnati <f.g.garbagnati@gmail.com>
2014-10-08 11:56:05 -0700

    grabbing the right data into appendMessageBox function

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,14 +1,30 @@
 $(document).ready( function() {
   $('form').on('ajax:success', function (e, data) {
     appendData( data )
-  })
+  });
 })
 
-appendData = function( data ) {
-  // $( 'table' ).append('<tr><td><a href="#">' + data.name + '</a></td></tr>')
+function appendData( data ) {
   var source = $('#table-template').html();
   var template = Handlebars.compile(source);
   var placeHolder = $('table');
   var html = template(data);
   placeHolder.append(html);
+  setLinkListeners();
+}
+
+function setLinkListeners() {
+  $('.table-link').on('click', triggerMessageBox);
+}
+
+function triggerMessageBox(e) {
+  e.preventDefault();
+  var name = this.innerHTML
+  var tableRow = this.parentElement.parentElement
+  var number = tableRow.children[2].innerHTML
+  appendMessageBox(name, number);
+}
+
+function appendMessageBox(name, number) {
+  debugger
 }
